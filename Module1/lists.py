c588c8b2eecadc6836dec64ab80252b9f29e4007
Blend Osmani <blendosmani010@gmail.com>
2025-11-08 14:33:33 +0100

    Module 1

---
+++ Module1/lists.py
@@ -0,0 +1,31 @@
+to_do_list = ["math homework" , "clean the room" , "research for history" , "fun"]
+print(to_do_list[0])
+print(to_do_list[1])
+print(to_do_list[2])
+print(to_do_list[3])
+
+shopping_list = ["apples" , "bananas", 3,4.5]
+shopping_list.append("oranges")
+
+print(shopping_list)
+
+shopping_list.insert(2,"lemons")
+print(shopping_list)
+
+to_do_list.remove("research for history")
+print(to_do_list)
+
+del to_do_list[2]
+print(to_do_list)
+
+to_do_list[0] = "physics homework"
+print(to_do_list)
+
+to_do_list[1] = 6654654
+print(to_do_list)
+
+
+
+
+
+
---
+++ Module1/variables.py
@@ -0,0 +1,22 @@
+name = "Blendi"
+print(name)
+
+temp = 38.5
+print(98.6)
+print(type(temp))
+
+age = 17
+print("Age is:", age)
+
+x = 5
+y = 234
+result = x + y
+print("Rezultati eshte:", result)
+
+age = age + 1
+print("Age now is:", age)
+
+first_name = "Blend"
+last_name = "Osmani"
+full_name = first_name + " " + last_name
+print(full_name)
